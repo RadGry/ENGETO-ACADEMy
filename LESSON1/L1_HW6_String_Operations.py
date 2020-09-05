86e78090ab0f31f9e5440addca032b2e3195cf40
Radek Gryc <radek.gryc@gmail.com>
2020-09-05 23:30:52 +0200

    Version 1

---
+++ LESSON1/L1_HW6_String_Operations.py
@@ -0,0 +1,30 @@
+# Save name
+name = str(input('Please enter you name:'))
+
+# Print name
+
+print("Saving '",name,"' into the name...")
+
+# Save surname
+
+surname = str(input('Please enter your surname:'))
+# Print surname
+
+print("Saving '",surname,"' into the surname...")
+
+# Create and print variable full_name
+
+full_name = name + ' ' + surname
+
+print(full_name)
+
+# Create and print variable name_length
+
+name_length = len(full_name)
+print('The length of full name is:', name_length)
+
+# Print bounded variable full_name
+
+print('='*int(name_length))
+print(full_name)
+print('='*int(name_length))
