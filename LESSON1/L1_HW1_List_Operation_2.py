ceed9122997e2d6d0e4102fa883171dd974c585e
Radek Gryc <radek.gryc@gmail.com>
2020-09-06 21:16:00 +0200

    Version 1

---
+++ LESSON1/L1_HW1_List_Operation_2.py
@@ -0,0 +1,23 @@
+# Results from the previous task
+candidates = ['Agnes', 'Agnes', 'Agnes']
+employees = ['Francis', 'Bruno', 'Ann', 'Jacob', 'Claire', 'Agnes', 'Agnes', 'Agnes']
+
+
+# Interval 2-5
+
+print( 'In interval from 2 to 5 is:', employees[2:6])
+
+# Each 3rd
+
+print('Every third member is: ', employees[::3])
+
+# Save index
+
+print('Jacob is at the index: ', employees.index('Jacob'))
+
+# Jacob index
+
+
+# Number of name Agnes
+
+print('Number of names Agnes in the the list candidates: ', employees.count('Agnes'))
