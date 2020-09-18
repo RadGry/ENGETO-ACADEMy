55a38cfc6621123f541c957bc026752d17fdc373
Radek Gryc <radek.gryc@gmail.com>
2020-09-18 22:35:02 +0200

    Final

---
+++ LESSON2/L2_HW13_Year_of_Birth.py
@@ -0,0 +1,10 @@
+# ask the user of age
+
+age = input(print('How old are you:'))
+
+# calculate and print YOB
+
+from datetime import date
+current_date = date.today()
+YOB = current_date.year - int(age)
+print('You were probably born in: ', + YOB)
