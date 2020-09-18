3a209dc813eadc2c72e947a318160dd0f91f616f
Radek Gryc <radek.gryc@gmail.com>
2020-09-19 01:34:05 +0200

    First and Final

---
+++ LESSON2/L2_HW16_BUGS_UPPERS.py
@@ -0,0 +1,10 @@
+some_str = str(input('Hello, please enter anything you want: '))
+
+if some_str.isupper():
+    print('All letters in your input are uppercase')
+
+elif some_str.islower():
+    print('All letters in your input are lowercase')
+
+else:
+    print('There are both uppercase and lowercase letters')
