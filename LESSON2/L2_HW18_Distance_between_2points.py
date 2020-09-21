0b8b48bcc792e7797854f4e6933b71dea3a9f924
Radek Gryc <radek.gryc@gmail.com>
2020-09-21 21:56:07 +0200

    First and Final

---
+++ LESSON2/L2_HW18_Distance_between_2points.py
@@ -0,0 +1,12 @@
+# input
+
+ax = int(input('Bod A - zadejte souřadnici X')
+
+ay = int(input('Bod A - zařidte souřadnici Y')
+
+bx = int(input('Bod B - zadejte souřadnici X')
+by = int(input('Bod B - zadejte souřadnici Y')
+
+# distance calculation
+
+distance = sqrt(abs((ax - bx)*(ay - by)))
